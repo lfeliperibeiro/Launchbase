8b398e4d53d8c32ec2344116b9c59c7270f1121f
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-16 11:03:48 -0300

    Update desafio aposentadoria

--- desafio_aposentadoria.js
+++ desafio_aposentadoria.js
@@ -1,11 +1,14 @@
 const nome = "Paula";
-const sexo = "F";
-const idade = 58;
-const contribuicao = 20;
+const sexo = 'F';
+const idade = 48;
+const contribuicao = 29;
 
 const regra = idade + contribuicao;
+const contribuicaoMinima = contribuicao >= 35 && sexo == 'M' || contribuicao >= 30 && sexo == 'F'
+const tempoMinimo = regra >= 95 && sexo == 'M' || regra >= 85 && sexo == 'F'
 
-if ((regra >= 95 && sexo == "M") || (regra >= 85 && sexo == "F")) {
+
+if (contribuicaoMinima || tempoMinimo) {
   console.log(`${nome}, você pode se aposentar`);
 } else {
   console.log(`${nome}, você não pode se aposentar`);
