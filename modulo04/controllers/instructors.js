8074d8a96e1c79116c2515625419ba29ceaca8ae
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-06 20:03:28 -0300

    update

--- modulo04/controllers/instructors.js
+++ modulo04/controllers/instructors.js
@@ -1,32 +1,15 @@
 const fs = require("fs");
-const data = require("./data.json");
+const data = require("../data.json");
 const Intl = require("intl");
-const { age, date } = require("./utils");
+const { age, date } = require("../utils");
 
 exports.index = function (req, res) {
   return res.render("instructors/index", { instructors: data.instructors });
 };
-// show
 
-exports.show = function (req, res) {
-  const { id } = req.params;
-  const foundInstructor = data.instructors.find(function (instructor) {
-    return instructor.id == id;
-  });
-  if (!foundInstructor) return res.send("instructor not found");
-
-  const instructor = {
-    ...foundInstructor,
-    birth: age(foundInstructor.birth),
-    services: foundInstructor.services.split(", "),
-    created_at: new Intl.DateTimeFormat("pt-BR").format(
-      foundInstructor.created_at
-    ),
-  };
-  return res.render("instructors/show", { instructor });
+exports.create = function (req, res) {
+  return res.render("instructors/create");
 };
-
-// create
 
 exports.post = function (req, res) {
   const keys = Object.keys(req.body);
@@ -60,7 +43,23 @@ exports.post = function (req, res) {
   //   return res.send(req.body);
 };
 
-// edit
+exports.show = function (req, res) {
+  const { id } = req.params;
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
+  if (!foundInstructor) return res.send("instructor not found");
+
+  const instructor = {
+    ...foundInstructor,
+    birth: age(foundInstructor.birth),
+    services: foundInstructor.services.split(", "),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(
+      foundInstructor.created_at
+    ),
+  };
+  return res.render("instructors/show", { instructor });
+};
 
 exports.edit = function (req, res) {
   const { id } = req.params;
@@ -71,13 +70,11 @@ exports.edit = function (req, res) {
 
   const instructor = {
     ...foundInstructor,
-    birth: date(foundInstructor.birth),
+    birth: date(foundInstructor.birth).iso,
   };
   date(foundInstructor.birth);
   return res.render("instructors/edit", { instructor });
 };
-
-// put
 
 exports.put = function (req, res) {
   const { id } = req.body;
@@ -97,7 +94,7 @@ exports.put = function (req, res) {
     ...foundInstructor,
     ...req.body,
     birth: Date.parse(req.body.birth),
-    id: Number(req.body.id)
+    id: Number(req.body.id),
   };
   data.instructors[index] = instructor;
 
