4196a850f027df5da1d0cd5ec76b7a4c0e945d5c
lfeliperibeiro <piporibeiro@gmail.com>
2020-07-16 19:26:15 -0300

    update

--- modulo05/src/app/models/Instructor.js
+++ modulo05/src/app/models/Instructor.js
@@ -3,7 +3,7 @@ const { age, date } = require("../../lib/utils");
 
 module.exports = {
   all(callback) {
-    db.query(`SELECT * FROM instructors`, (err, results) => {
+    db.query(`SELECT * FROM instructors ORDER BY name ASC`, (err, results) => {
       if (err) `Database error ${err}`;
 
       callback(results.rows);
