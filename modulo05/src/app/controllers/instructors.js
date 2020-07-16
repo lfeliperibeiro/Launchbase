509fa5efea3481c70fbf0799a01714c7175965df
lfeliperibeiro <piporibeiro@gmail.com>
2020-07-15 23:41:46 -0300

    update db

--- modulo05/src/app/controllers/instructors.js
+++ modulo05/src/app/controllers/instructors.js
@@ -1,13 +1,11 @@
 const Intl = require("intl");
 const { age, date } = require("../../lib/utils");
-const db = require("../../config/db");
+const Instructor = require("../models/Intructor");
 
 module.exports = {
   index(req, res) {
-    db.query(`SELECT * FROM instructors`, (err, results) => {
-      if (err) res.send("Database error");
-
-      return res.render("instructors/index", { instructors: results.rows });
+    Instructor.all((instructors) => {
+      return res.render("instructors/index", { instructors });
     });
   },
   create(req, res) {
@@ -20,35 +18,9 @@ module.exports = {
         return res.send("Please, fill all fields");
       }
     }
-
-    const query = `
-    INSERT INTO instructors(
-      name,
-      avatar_url,
-      gender,
-      services,
-      birth,
-      created_at
-    )VALUES($1,$2,$3,$4,$5,$6)
-    RETURNING id
-    `;
-
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso,
-      date(Date.now()).iso,
-    ];
-
-    db.query(query, values, (err, results) => {
-      if (err) res.send("Database error");
-
-      return res.redirect(`/instructors/${results.rows[0].id}`);
+    Instructor.create(req.body, (instructor) => {
+      return res.redirect(`/instructors/${instructor.id}`);
     });
-
-    return;
   },
   show(req, res) {
     return;
