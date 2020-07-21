eeb61edfa6ebaa2a0b70294370bc1e46ea37fde5
lfeliperibeiro <piporibeiro@gmail.com>
2020-07-20 23:33:52 -0300

    create pagination logic

--- modulo05/src/app/controllers/instructors.js
+++ modulo05/src/app/controllers/instructors.js
@@ -4,16 +4,32 @@ const Instructor = require("../models/Instructor");
 
 module.exports = {
   index(req, res) {
-    const { filter } = req.query;
-    if (filter) {
-      Instructor.findBy(filter, (instructors) => {
-        return res.render("instructors/index", { instructors, filter });
-      });
-      return;
+    let { filter, page, limit } = req.query;
+
+    page = page || 1
+    limit = limit|| 2
+    let offset = limit * (page - 1)
+    
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(instructors){
+          return res.render("instructors/index", { instructors, filter });
     }
-    Instructor.all((instructors) => {
-      return res.render("instructors/index", { instructors });
-    });
+  }
+  
+   Instructor.paginate(params)
+
+    // if (filter) {
+    //   Instructor.findBy(filter, (instructors) => {
+    //   });
+    //   return;
+    // }
+    // Instructor.all((instructors) => {
+    //   return res.render("instructors/index", { instructors });
+    // });
   },
   create(req, res) {
     return res.render("instructors/create");
