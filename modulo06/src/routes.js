03c10ac9f4a42de7577794668862233b7b9a4d34
lfeliperibeiro <piporibeiro@gmail.com>
2020-07-24 20:38:16 -0300

    update

--- modulo06/src/routes.js
+++ modulo06/src/routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const routes = express.Router();
+const productController = require("./app/controllers/productController")
 
 routes.get("/", (req, res) => {
   return res.render("layout.njk");
 });
+
+routes.get("/products/create", productController.create) 
+
+routes.get("/ads/create", (req, res)=>{
+    return res.redirect("/products/create")
+})
 
 module.exports = routes;
