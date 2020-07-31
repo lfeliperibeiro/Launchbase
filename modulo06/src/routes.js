b2e9af97c9fe5b30f184d54f3bf4ca715edafdf1
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-07-30 23:27:40 -0300

    create post

--- modulo06/src/routes.js
+++ modulo06/src/routes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const routes = express.Router();
-const productController = require("./app/controllers/productController")
+const productController = require("./app/controllers/productController");
 
 routes.get("/", (req, res) => {
   return res.render("layout.njk");
 });
 
-routes.get("/products/create", productController.create) 
+routes.get("/products/create", productController.create);
+routes.post("/products", productController.post);
 
-routes.get("/ads/create", (req, res)=>{
-    return res.redirect("/products/create")
-})
+routes.get("/ads/create", (req, res) => {
+  return res.redirect("/products/create");
+});
 
 module.exports = routes;
