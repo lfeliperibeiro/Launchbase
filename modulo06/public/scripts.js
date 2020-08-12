d818411daad98a59fef5ca55c11b55578ac29293
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-12 18:25:38 -0300

    create logic of image limits

--- modulo06/public/scripts.js
+++ modulo06/public/scripts.js
@@ -16,16 +16,46 @@ const Mask = {
 
 
 const photosUpload ={
+    preview:  document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event){        
         const {files: fileList} = event.target
+        
+        if(photosUpload.hasLimit(event)) return
+
+        Array.from(fileList).forEach(file => {
+           const reader = new FileReader()
+           
+           reader.onload = () =>{
+                const image = new Image()
+                image.src = String(reader.result) 
+
+                const div = photosUpload.getContainer(image)
+                photosUpload.preview.appendChild(div)               
+           }
+           reader.readAsDataURL(file)
+        })
+    },
+
+    hasLimit(event){
         const {uploadLimit} = photosUpload
         if(fileList.length > uploadLimit){
             alert(`Envie no máximp ${uploadLimit} fotos`)
             event.preventDefault()
-            return
+            return true
         }
 
-        
+        return false
+    },
+
+    getContainer(image){
+        const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('remover foto')
+
+                div.appendChild(image)
+
+                return div
     }
 }
