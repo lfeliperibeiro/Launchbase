caf281ab6c71f3d5613242a89d1a0b86b1edd761
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-08-19 19:31:06 -0300

    update

--- modulo06/src/app/controllers/productController.js
+++ modulo06/src/app/controllers/productController.js
@@ -4,9 +4,6 @@ const Category = require("../models/Category");
 const Product = require("../models/Product");
 const File = require("../models/File");
 
-
-
-
 module.exports = {
   create(request, response) {
     Category.all()
