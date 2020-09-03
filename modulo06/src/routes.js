225ac4d0cee4c5fe4d6d967cc5351badde91a73b
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-09-03 17:20:39 -0300

    create search page

--- modulo06/src/routes.js
+++ modulo06/src/routes.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const routes = express.Router();
 const multer = require('./app/middlewares/multer')
-const productController = require("./app/controllers/productController");
+const ProductController = require("./app/controllers/ProductController");
 const HomeController = require("./app/controllers/HomeController");
+const SearchController = require("./app/controllers/SearchController");
 
 routes.get("/", HomeController.index);
 
-routes.get("/products/create", productController.create);
-routes.get("/products/:id", productController.show);
-routes.get("/products/:id/edit", productController.edit);
+routes.get('/products/search', SearchController.index);
 
-routes.post("/products", multer.array("photos", 6), productController.post);
-routes.put("/products", multer.array("photos", 6), productController.put);
+routes.get("/products/create", ProductController.create);
+routes.get("/products/:id", ProductController.show);
+routes.get("/products/:id/edit", ProductController.edit);
 
-routes.delete("/products", productController.delete);
+routes.post("/products", multer.array("photos", 6), ProductController.post);
+routes.put("/products", multer.array("photos", 6), ProductController.put);
+
+routes.delete("/products", ProductController.delete);
 
 routes.get("/ads/create", (request, response) => {
   return response.redirect("/products/create");
 });
+
 
 module.exports = routes;
