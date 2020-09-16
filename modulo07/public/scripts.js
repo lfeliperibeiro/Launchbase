36c93a7f50a7e6e908e673fc33f776907c54b895
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-09-15 21:23:10 -0300

    create validade inputs

--- modulo07/public/scripts.js
+++ modulo07/public/scripts.js
@@ -178,3 +178,62 @@ const Lightbox = {
 
 
 }
+
+const Validate = {
+    apply(input, func) {
+        Validate.clearError(input)
+        let results = Validate[func](input.value)
+        input.value = results.value
+
+        if(results.error)
+            Validate.displayError(input, results.error)            
+    },
+    displayError(input, error) {
+      const div = document.createElement('div')
+      div.classList.add('error')
+      div.innerHTML = error
+      input.parentNode.appendChild(div)
+      input.focus()
+    },
+    clearError(input) {
+        const errorDiv = input.parentNode.querySelector(".error")
+        if(errorDiv) 
+        errorDiv.remove()
+    },
+    isEmail(value) {
+        let error = null
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if(!value.match(mailFormat))
+        error = "Email invalido!"
+        return {
+            error,
+            value
+        }
+    },
+    isCpfCnpj(value){
+        let error = null
+        const cleanValues = value.replace(/\D/g, "")
+        // 11.111.111/1111-1
+        if(cleanValues.length > 11 && cleanValues.length !== 13) {
+            error = "CNPJ incorreto!"
+        }else if(cleanValues.length < 11 && cleanValues.length !== 15){
+            error = "CPF incorreto"
+        }
+        return {
+            error,
+            value
+        }
+    },
+    isCep(value) {
+        let error = null
+        const cleanValues = value.replace(/\D/g, "")
+        if(cleanValues.length !== 8){
+            error = "CEP incorreto"
+        }
+        return {
+            error,
+            value
+        }
+    }
+}
