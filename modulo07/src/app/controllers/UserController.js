6c79c57cd65bf3b91adbbd30a1cf98cefd14ac8c
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-09-16 20:02:52 -0300

    create validate password

--- modulo07/src/app/controllers/UserController.js
+++ modulo07/src/app/controllers/UserController.js
@@ -1,7 +1,31 @@
-
+const User = require('../models/User');
 
 module.exports = {
     registerForm(request, response) {
         return response.render('users/register')
+    },
+    async post(request, response) {
+        const keys = Object.keys(request.body);
+
+        for(key of keys){
+            if(request.body[key] == '') {
+                return response.send('Please, fill all fileds')
+            }
+        }
+
+        let {email, cpf_cnpj, password, passwordRepeat} = request.body;
+        
+        const user = await User.findOne({
+            where: { email},
+            or: {cpf_cnpj}
+        })
+        if(user) return response.send('User exists')
+
+        if(password != passwordRepeat)
+        return response.send('Password Mismatch')
+
+        return response.send('Passed!')
+
+
     }
 }
